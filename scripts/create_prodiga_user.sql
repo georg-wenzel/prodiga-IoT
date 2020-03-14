3f4e3a7878fab6eadf657010d4abb736edabb30f
Maximilian Suitner <maximilian.suitner@student.uibk.ac.at>
2020-03-14 17:15:33 +0100

    Changed SQL Script and DDL creation :beers:

--- scripts/create_prodiga_user.sql
+++ scripts/create_prodiga_user.sql
@@ -1,3 +1,4 @@
 CREATE USER 'prodiga'@'localhost' IDENTIFIED BY 'SuperGeheimesPassword123NacktNiemand!!';
 GRANT ALL PRIVILEGES ON * . * TO 'prodiga'@'localhost';
 FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS prodiga;
