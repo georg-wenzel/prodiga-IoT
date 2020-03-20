a911b7ea9541aa195b5335860be01ef09ace5734
Jamie <jamie.hochrainer@student.uibk.ac.at>
2020-03-20 20:00:19 +0100

    RoomServiceTest afterPropertiesSet

--- src/test/java/uibk/ac/at/prodiga/tests/RoomServiceTest.java
+++ src/test/java/uibk/ac/at/prodiga/tests/RoomServiceTest.java
@@ -1,5 +1,6 @@
 package uibk.ac.at.prodiga.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.InitializingBean;
@@ -9,12 +10,19 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import uibk.ac.at.prodiga.model.Room;
+import uibk.ac.at.prodiga.model.User;
 import uibk.ac.at.prodiga.repositories.RoomRepository;
 import uibk.ac.at.prodiga.repositories.UserRepository;
 import uibk.ac.at.prodiga.services.RoomService;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @WebAppConfiguration
@@ -29,10 +37,38 @@ public class RoomServiceTest implements InitializingBean {
     @Autowired
     UserRepository userRepository;
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        //Grab admin user to set as creation user for test room
+        User admin = userRepository.findFirstByUsername("admin");
+
+        //Before tests, initialize test room
+        Room test_room1 = new Room();
+        test_room1.setObjectCreatedUser(admin);
+        test_room1.setObjectCreatedDateTime(new Date());
+        test_room1.setName("test_room_1");
+        test_room1 = roomRepository.save(test_room1);
+
+        Room test_room2 = new Room();
+        test_room2.setObjectCreatedUser(admin);
+        test_room2.setObjectCreatedDateTime(new Date());
+        test_room2.setName("test_room_2");
+        test_room2 = roomRepository.save(test_room2);
+
+
+    }
+
     @DirtiesContext
     @Test
     @WithMockUser(username = "admin", authorities = {"ADMIN"})
-    void getAllRooms() {
+    public void getAllRooms() {
+
+
+        List rooms = new ArrayList();
+        rooms.add(test_room1);
+        rooms.add(test_rooms2);
+        Assertions.assertEquals(roomService.getAllRooms(),rooms);
+
     }
 
     @DirtiesContext
@@ -47,8 +83,6 @@ public class RoomServiceTest implements InitializingBean {
     void deleteRoom() {
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
 
-    }
 }
+*/
