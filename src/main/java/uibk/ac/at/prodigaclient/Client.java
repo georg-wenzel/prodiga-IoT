090ff0c3f43e314104f32b1102b7a79f6662dcb6
Maximilian Suitner <maximilian.suitner@student.uibk.ac.at>
2020-03-22 19:26:05 +0100

    Client

--- src/main/java/uibk/ac/at/prodigaclient/Client.java
+++ src/main/java/uibk/ac/at/prodigaclient/Client.java
@@ -1,7 +1,36 @@
 package uibk.ac.at.prodigaclient;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import uibk.ac.at.prodiga.rest.dtos.GenericStringDTO;
+import uibk.ac.at.prodigaclient.api.IntrinsicsControllerApi;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hallo, ich bin ein client!!!");
+
+        CountDownLatch startSignal = new CountDownLatch(1);
+
+        IntrinsicsControllerApi api = new ApiClient().createService(IntrinsicsControllerApi.class);
+
+        api.pingUsingGET().enqueue(new Callback<GenericStringDTO>() {
+            @Override
+            public void onResponse(Call<GenericStringDTO> call, Response<GenericStringDTO> response) {
+                System.out.println(response.body());
+                startSignal.countDown();
+            }
+
+            @Override
+            public void onFailure(Call<GenericStringDTO> call, Throwable throwable) {
+                System.out.println(throwable.toString());
+                startSignal.countDown();
+            }
+        });
+
+        startSignal.await();
     }
 }
---
+++ src/main/java/uibk/ac/at/prodigaclient/api/IntrinsicsControllerApi.java
@@ -0,0 +1,42 @@
+package uibk.ac.at.prodigaclient.api;
+
+import uibk.ac.at.prodiga.rest.dtos.GenericStringDTO;
+import uibk.ac.at.prodiga.rest.dtos.InstrincsDTO;
+import uibk.ac.at.prodigaclient.CollectionFormats.*;
+
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public interface IntrinsicsControllerApi {
+  /**
+   * ping
+   * 
+   * @return Call&lt;GenericStringDTO&gt;
+   */
+  @GET("api/ping")
+  Call<GenericStringDTO> pingUsingGET();
+    
+
+  /**
+   * push
+   * 
+   * @param body instrincs (required)
+   * @return Call&lt;Void&gt;
+   */
+  @Headers({
+    "Content-Type:application/json"
+  })
+  @POST("api/instrincs")
+  Call<Void> pushUsingPOST(
+                    @retrofit2.http.Body InstrincsDTO body
+  );
+
+}
