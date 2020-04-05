6c1f9fc2befebad36f6cf5ee14120bac91d9bb9f
Laura Geiger <laura.geiger@student.uibk.ac.at>
2020-04-05 16:36:46 +0200

    some additions for raspi view

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,8 +16,11 @@ INSERT INTO user_user_role (user_username, ROLES) VALUES ('ITS leader', 'DEPARTM
 INSERT INTO user (enabled, first_name, last_name, password, username, create_user_username, create_date, assigned_department_id) VALUES(TRUE, 'Sandra', 'Sun', '$2a$10$d8cQ7Euz2hM43HOHWolUGeCEZSS/ltJVJYiJAmczl1X5FKzCjg6PC', 'RD leader', 'admin', '2016-01-01 00:00:00', 2);
 INSERT INTO user_user_role (user_username, ROLES) VALUES ('RD leader', 'DEPARTMENTLEADER');
 
-INSERT INTO room (ID, NAME, object_created_date_time, object_created_user_username) VALUE (1, 'Test Room 1', CURRENT_TIMESTAMP, 'admin');
-INSERT INTO room (ID, NAME, object_created_date_time, object_created_user_username) VALUE (2, 'Test Room 2', CURRENT_TIMESTAMP, 'admin');
-INSERT INTO room (ID, NAME, object_created_date_time, object_created_user_username) VALUE (3, 'Test Room 3', CURRENT_TIMESTAMP, 'admin');
 
-INSERT IGNORE INTO raspberry_pi (id, internal_id, object_changed_date_time, object_created_date_time, password, object_changed_user_username, object_created_user_username) VALUES (1, 'test', NOW(), NOW(), '$2a$10$d8cQ7Euz2hM43HOHWolUGeCEZSS/ltJVJYiJAmczl1X5FKzCjg6PC', 'admin', 'admin');
+INSERT INTO room (ID, NAME, object_created_date_time, object_changed_date_time, object_created_user_username, object_changed_user_username) VALUE (1, 'Test Room 1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'admin', 'admin');
+INSERT INTO room (ID, NAME, object_created_date_time, object_changed_date_time, object_created_user_username, object_changed_user_username) VALUE (2, 'Test Room 2', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'admin', 'admin');
+INSERT INTO room (ID, NAME, object_created_date_time, object_changed_date_time, object_created_user_username, object_changed_user_username) VALUE (3, 'Test Room 3', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'admin', 'admin');
+
+INSERT INTO room (ID, NAME, object_created_date_time, object_changed_date_time, object_created_user_username, object_changed_user_username) VALUE (7, 'Test Room 7', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'admin', 'admin');
+
+INSERT INTO raspberry_pi (id, internal_id, object_changed_date_time, object_created_date_time, password, object_changed_user_username, object_created_user_username, assigned_room_id) VALUES (1, 'test 1', NOW(), NOW(), '$2a$10$d8cQ7Euz2hM43HOHWolUGeCEZSS/ltJVJYiJAmczl1X5FKzCjg6PC', 'admin', 'admin', 7);
