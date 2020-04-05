7988b849744b84bb5cde3572046970eedfa515ca
Jamie <jamie.hochrainer@student.uibk.ac.at>
2020-04-05 11:55:18 +0200

    sql insert room

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,8 +7,6 @@ INSERT INTO user_user_role (user_username, ROLES) VALUES ('user1', 'EMPLOYEE');
 INSERT INTO user (enabled, first_name, last_name, password, username, create_user_username, create_date) VALUES(TRUE, 'Max', 'Mustermann', '$2a$10$d8cQ7Euz2hM43HOHWolUGeCEZSS/ltJVJYiJAmczl1X5FKzCjg6PC', 'user2', 'admin', '2016-01-01 00:00:00');
 INSERT INTO user_user_role (user_username, ROLES) VALUES ('user2', 'EMPLOYEE');
 
-
-
 INSERT INTO department (ID, NAME, object_created_date_time, object_created_user_username) VALUES (1, 'IT services',CURRENT_TIMESTAMP, 'admin');
 INSERT INTO department (ID, NAME, object_created_date_time, object_created_user_username) VALUES (2, 'Research and development',CURRENT_TIMESTAMP, 'admin');
 INSERT INTO department (ID, NAME, object_created_date_time, object_created_user_username) VALUES (3, 'Testing and development',CURRENT_TIMESTAMP, 'admin');
@@ -17,5 +15,9 @@ INSERT INTO user (enabled, first_name, last_name, password, username, create_use
 INSERT INTO user_user_role (user_username, ROLES) VALUES ('ITS leader', 'DEPARTMENTLEADER');
 INSERT INTO user (enabled, first_name, last_name, password, username, create_user_username, create_date, assigned_department_id) VALUES(TRUE, 'Sandra', 'Sun', '$2a$10$d8cQ7Euz2hM43HOHWolUGeCEZSS/ltJVJYiJAmczl1X5FKzCjg6PC', 'RD leader', 'admin', '2016-01-01 00:00:00', 2);
 INSERT INTO user_user_role (user_username, ROLES) VALUES ('RD leader', 'DEPARTMENTLEADER');
+
+INSERT INTO room (ID, NAME, object_created_date_time, object_created_user_username) VALUE (1, 'Test Room 1', CURRENT_TIMESTAMP, 'admin');
+INSERT INTO room (ID, NAME, object_created_date_time, object_created_user_username) VALUE (2, 'Test Room 2', CURRENT_TIMESTAMP, 'admin');
+INSERT INTO room (ID, NAME, object_created_date_time, object_created_user_username) VALUE (3, 'Test Room 3', CURRENT_TIMESTAMP, 'admin');
 
 INSERT IGNORE INTO raspberry_pi (id, internal_id, object_changed_date_time, object_created_date_time, password, object_changed_user_username, object_created_user_username) VALUES (1, 'test', NOW(), NOW(), '$2a$10$d8cQ7Euz2hM43HOHWolUGeCEZSS/ltJVJYiJAmczl1X5FKzCjg6PC', 'admin', 'admin');
