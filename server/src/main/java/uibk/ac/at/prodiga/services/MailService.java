352ec27a736b3fcb8a9f798dfba42d670cf7c309
Jamie <jamie.hochrainer@student.uibk.ac.at>
2020-04-10 17:09:36 +0200

    mail service

---
+++ server/src/main/java/uibk/ac/at/prodiga/services/MailService.java
@@ -0,0 +1,28 @@
+package uibk.ac.at.prodiga.services;
+//from last years project
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import uibk.ac.at.prodiga.model.User;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+    public void sendMail(String toAddress, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toAddress);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+
+    public void sendEmailTo(User user, String subject, String text) {
+        if (user.getEmail() != null && !user.getEmail().isEmpty()) {
+            sendMail(user.getEmail(), subject, text);
+        }
+    }
+
+}
