7a087cba01776329ce94f6f97c43e01948b7a920
Laura Geiger <laura.geiger@student.uibk.ac.at>
2020-04-24 20:36:46 +0200

    Badge repos

---
+++ server/src/main/java/uibk/ac/at/prodiga/repositories/BadgeDBRepository.java
@@ -0,0 +1,20 @@
+package uibk.ac.at.prodiga.repositories;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import uibk.ac.at.prodiga.model.BadgeDB;
+import uibk.ac.at.prodiga.model.User;
+
+import java.util.Collection;
+
+/**
+ * DB Repository for managing Badges
+ */
+
+public interface BadgeDBRepository extends AbstractRepository<BadgeDB, String> {
+
+    BadgeDB findFirstByUsername(String badgeName);
+
+    @Query("SELECT ALL FROM BadgeDB b WHERE :user = b.user")
+    Collection<BadgeDB> findByUser(@Param("user") User user);
+}
