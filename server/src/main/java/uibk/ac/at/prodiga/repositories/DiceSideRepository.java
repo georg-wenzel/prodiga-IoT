355e95e012681bba918820585ebb7502daafae97
Georg Felix Wenzel <georg.wenzel@student.uibk.ac.at>
2020-04-25 16:44:31 +0200

    configuring dice sides

--- server/src/main/java/uibk/ac/at/prodiga/repositories/DiceSideRepository.java
+++ server/src/main/java/uibk/ac/at/prodiga/repositories/DiceSideRepository.java
@@ -4,8 +4,10 @@ import uibk.ac.at.prodiga.model.Dice;
 import uibk.ac.at.prodiga.model.DiceSide;
 import uibk.ac.at.prodiga.services.DiceService;
 
+import java.util.Collection;
+
 public interface DiceSideRepository extends AbstractRepository<DiceSide, Long> {
 
     DiceSide findFirstByDiceAndSide(Dice d, int side);
-
+    Collection<DiceSide> findAllByDice(Dice d);
 }
--- server/src/main/java/uibk/ac/at/prodiga/ui/controllers/DiceConfigurationController.java
+++ server/src/main/java/uibk/ac/at/prodiga/ui/controllers/DiceConfigurationController.java
@@ -1,15 +1,19 @@
 package uibk.ac.at.prodiga.ui.controllers;
 
+import org.primefaces.PrimeFaces;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import uibk.ac.at.prodiga.model.*;
 import uibk.ac.at.prodiga.services.BookingCategoryService;
 import uibk.ac.at.prodiga.services.DiceService;
+import uibk.ac.at.prodiga.services.DiceSideService;
+import uibk.ac.at.prodiga.utils.DiceConfigurationWrapper;
+import uibk.ac.at.prodiga.utils.MessageType;
+import uibk.ac.at.prodiga.utils.ProdigaGeneralExpectedException;
 import uibk.ac.at.prodiga.utils.ProdigaUserLoginManager;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.security.KeyPair;
+import java.util.*;
 
 /**
  * Controller for managing user dice configuration
@@ -20,33 +24,61 @@ public class DiceConfigurationController
 {
     private final ProdigaUserLoginManager userLoginManager;
     private final DiceService diceService;
+    private final DiceSideService diceSideService;
     private final BookingCategoryService bookingCategoryService;
 
     private Dice dice;
+    private UUID uuid;
+    private int currentSide = -1;
     private boolean inConfiguration = false;
+    private DiceConfigurationWrapper wrapper;
 
-    private Map<Integer, DiceSide> sides;
+    private Map<Integer, BookingCategory> sides;
+    private Map<Integer, String> newSides;
 
-    public DiceConfigurationController(ProdigaUserLoginManager userLoginManager, DiceService diceService, BookingCategoryService bookingCategoryService)
+    public DiceConfigurationController(ProdigaUserLoginManager userLoginManager, DiceService diceService, DiceSideService diceSideService, BookingCategoryService bookingCategoryService)
     {
         this.userLoginManager = userLoginManager;
         this.diceService = diceService;
+        this.diceSideService = diceSideService;
         this.bookingCategoryService = bookingCategoryService;
 
         this.dice = this.diceService.getDiceByUser(userLoginManager.getCurrentUser());
-        sides = new HashMap<Integer, DiceSide>();
-        for(int i=1; i<=12; i++)
-        {
-            sides.put(i, null);
+        sides = new HashMap<Integer, BookingCategory>();
+        newSides = new HashMap<Integer, String>();
+
+        if(dice != null) {
+            Collection<DiceSide> currSides = diceSideService.findByDice(dice);
+            for (int i = 1; i <= 12; i++)
+            {
+                //because lambda expression should be final..
+                int finalI = i;
+
+                Optional<DiceSide> side = currSides.stream().filter(x -> x.getSide() == finalI).findFirst();
+                if (side.isPresent()) {
+                    sides.put(i, side.get().getBookingCategory());
+                    newSides.put(i, side.get().getBookingCategory().getId().toString());
+                }
+                else {
+                    sides.put(i, null);
+                    newSides.put(i, null);
+                }
+            }
         }
     }
 
     /**
      * Saves current dice configuration.
      */
-    public void confirmMapping()
+    public void confirmMapping() throws ProdigaGeneralExpectedException
     {
-
+        for(Map.Entry<Integer, String> kvp: newSides.entrySet())
+        {
+            if(kvp.getValue() == null) continue;
+            wrapper.getCompletedSides().put(kvp.getKey(), bookingCategoryService.findById(Long.parseLong(kvp.getValue())));
+        }
+        diceService.completeConfiguration(dice);
+        abortMapping();
     }
 
     /**
@@ -54,7 +86,16 @@ public class DiceConfigurationController
      */
     public void abortMapping()
     {
+        diceService.unregisterNewSideCallback(uuid);
+        inConfiguration = false;
+    }
 
+    /**
+     * Updates table view
+     */
+    private void updateView()
+    {
+        PrimeFaces.current().ajax().update(":diceConfigForm");
     }
 
     /**
@@ -81,16 +122,28 @@ public class DiceConfigurationController
         this.dice = dice;
     }
 
-    public void startConfigureDice()
+    public void startConfigureDice() throws ProdigaGeneralExpectedException
     {
+        if(dice == null) throw new ProdigaGeneralExpectedException("Dice Configuration cannot be started because no dice was found for the user.", MessageType.ERROR);
+
+        uuid = UUID.randomUUID();
+        wrapper = diceService.addDiceToConfiguration(dice);
+        currentSide = wrapper.getCurrentSide();
+
+        diceService.registerNewSideCallback(uuid, x -> {
+            if(x.getValue0() != uuid) return;
+
+            currentSide = x.getValue1().getCurrentSide();
+            this.updateView();
+        });
         inConfiguration = true;
     }
 
-    public Map<Integer, DiceSide> getSides() {
+    public Map<Integer, BookingCategory> getSides() {
         return sides;
     }
 
-    public void setSides(Map<Integer, DiceSide> sides) {
+    public void setSides(Map<Integer, BookingCategory> sides) {
         this.sides = sides;
     }
 
@@ -104,5 +157,22 @@ public class DiceConfigurationController
         {
             return bookingCategoryService.findAllCategoriesByTeam();
         }
+    }
+
+    public int getCurrentSide() {
+        return currentSide;
+    }
+
+    public BookingCategory getSide(int key)
+    {
+        return sides.get(key);
+    }
+
+    public Map<Integer, String> getNewSides() {
+        return newSides;
+    }
+
+    public void setNewSides(Map<Integer, String> newSides) {
+        this.newSides = newSides;
     }
 }
