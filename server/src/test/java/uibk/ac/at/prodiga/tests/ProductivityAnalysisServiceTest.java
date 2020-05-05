7a635209c8f1e7cee26637cf0c5179fb1ab27476
Jamie <jamie.hochrainer@student.uibk.ac.at>
2020-05-05 22:11:37 +0200

    Data setup tests, DataHelper

--- server/src/test/java/uibk/ac/at/prodiga/tests/ProductivityAnalysisServiceTest.java
+++ server/src/test/java/uibk/ac/at/prodiga/tests/ProductivityAnalysisServiceTest.java
@@ -1,6 +1,7 @@
 package uibk.ac.at.prodiga.tests;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.internal.util.collections.Sets;
@@ -60,48 +61,42 @@ public class ProductivityAnalysisServiceTest {
     @Autowired
     ProductivityAnalysisService productivityAnalysisService;
 
+    User admin = null;
+    User u1 = null;
+    User u2 = null;
+    Dice d = null;
+    BookingCategory cat1 = null;
+    BookingCategory cat2 = null;
+
+    @BeforeEach
+    public void initEach(@Autowired UserRepository userRepository){
+        admin = DataHelper.createAdminUser("admin", userRepository);
+        admin.setMayEditHistoricData(true);
+        admin = userRepository.save(admin);
+        u1 = DataHelper.createUserWithRoles("statistic_test_user1", Sets.newSet(UserRole.EMPLOYEE),userRepository);
+        u2 = DataHelper.createUserWithRoles("statistic_test_user2", Sets.newSet(UserRole.EMPLOYEE),userRepository);
+        d = DataHelper.createDice("testdice", null, admin, u1, diceRepository, raspberryPiRepository, roomRepository);
+        cat1 = DataHelper.createBookingCategory("test_category_01", admin, bookingCategoryRepository);
+        cat2 = DataHelper.createBookingCategory("test_category_02", admin, bookingCategoryRepository);
+    }
+
     @DirtiesContext
     @Test
     @WithMockUser(username = "statistic_test_user1", authorities = {"EMPLOYEE"})
     public void statistic_user_backstepDay1_1booking(){
-        //Data setup
-        User admin = DataHelper.createAdminUser("admin", userRepository);
-        User u1 = DataHelper.createUserWithRoles("statistic_test_user1", Sets.newSet(UserRole.EMPLOYEE), userRepository);
-        Dice d = DataHelper.createDice("testdice", null, admin, u1, diceRepository, raspberryPiRepository, roomRepository);
-        BookingCategory cat = DataHelper.createBookingCategory("test_category_01", admin, bookingCategoryRepository);
-        BookingCategory cat2 = DataHelper.createBookingCategory("test_category_02", admin, bookingCategoryRepository);
+        //end 1 hour ago
+        Date endingTime1 = new Date(new Date().getTime() - 60*1000*60);
+        //start 3 hours ago
+        Date startingTime1 = new Date(new Date().getTime() - 60*1000*180);
+        Booking booking1 = DataHelper.createBooking(cat1,startingTime1,endingTime1,u1,d,bookingRepository);
+        //end 25 days ago
+        Date endingTime2 = new Date(new Date().getTime() - 60*1000*60*25);
+        //start 50 days ago
+        Date startingTime2 = new Date(new Date().getTime() - 60*1000*60*30);
+        Booking booking2 = DataHelper.createBooking(cat2,startingTime2,endingTime2,u1,d,bookingRepository);
 
-        Booking b1 = new Booking();
-        b1.setDice(d);
-        b1.setBookingCategory(cat);
-        //set activity end time to 1 hour before current time.
-        Date endingTime = new Date(new Date().getTime() - 60*1000*60);
-        b1.setActivityEndDate(endingTime);
-        //set activity start time to 3 hours ago
-        Date startingTime = new Date(new Date().getTime() - 60*1000*180);
-        b1.setActivityStartDate(startingTime);
-        try {
-            b1 = bookingService.saveBooking(b1);
-        } catch (ProdigaGeneralExpectedException e) {
-            e.printStackTrace();
-        }
-
-        Booking b2 = new Booking();
-        b2.setDice(d);
-        b2.setBookingCategory(cat2);
-        //set activity end time to 25 hours before current time.
-        Date endingTime1 = new Date(new Date().getTime() - 60*1000*60*25);
-        b2.setActivityEndDate(endingTime1);
-        //set activity start time to 30 hours ago
-        Date startingTime1 = new Date(new Date().getTime() - 60*1000*60*30);
-        b2.setActivityStartDate(startingTime1);
-        try {
-            b2 = bookingService.saveBooking(b2);
-        } catch (ProdigaGeneralExpectedException e) {
-            e.printStackTrace();
-        }
         HashMap<BookingCategory,Long> dailyStatistic = new HashMap<>();
-        dailyStatistic.put(cat, (endingTime.getTime()-startingTime.getTime()) / (1000*60*60));
+        dailyStatistic.put(cat1, (endingTime1.getTime()-startingTime1.getTime()) / (1000*60*60));
         Assertions.assertEquals(dailyStatistic, productivityAnalysisService.getStatisicForCurrentUserByDay(1));
 
     }
@@ -110,88 +105,39 @@ public class ProductivityAnalysisServiceTest {
     @Test
     @WithMockUser(username = "statistic_test_user1", authorities = {"EMPLOYEE"})
     public void statistic_user_backstepDay1_2bookings(){
-        //Data setup
-        User admin = DataHelper.createAdminUser("admin", userRepository);
-        User u1 = DataHelper.createUserWithRoles("statistic_test_user1", Sets.newSet(UserRole.EMPLOYEE), userRepository);
-        Dice d = DataHelper.createDice("testdice", null, admin, u1, diceRepository, raspberryPiRepository, roomRepository);
-        BookingCategory cat1 = DataHelper.createBookingCategory("test_category_01", admin, bookingCategoryRepository);
-        BookingCategory cat2 = DataHelper.createBookingCategory("test_category_02", admin, bookingCategoryRepository);
-
-        Booking b1 = new Booking();
-        b1.setDice(d);
-        b1.setBookingCategory(cat1);
         //set activity end time to 1 hour before current time.
         Date endingTime1 = new Date(new Date().getTime() - 60*1000*60);
-        b1.setActivityEndDate(endingTime1);
         //set activity start time to 3 hours ago
         Date startingTime1 = new Date(new Date().getTime() - 60*1000*180);
-        b1.setActivityStartDate(startingTime1);
-        try {
-            b1 = bookingService.saveBooking(b1);
-        } catch (ProdigaGeneralExpectedException e) {
-            e.printStackTrace();
-        }
-        Booking b2 = new Booking();
-        b2.setDice(d);
-        b2.setBookingCategory(cat2);
+        Booking booking1 = DataHelper.createBooking(cat1,startingTime1,endingTime1,u1,d,bookingRepository);
+
         //set activity end time to 5 minutes before current time.
         Date endingTime2 = new Date(new Date().getTime() - 60*1000*5);
-        b2.setActivityEndDate(endingTime2);
         //set activity start time to 30 minutes ago
         Date startingTime2 = new Date(new Date().getTime() - 60*1000*30);
-        b2.setActivityStartDate(startingTime2);
-        try {
-            b2 = bookingService.saveBooking(b2);
-        } catch (ProdigaGeneralExpectedException e) {
-            e.printStackTrace();
-        }
+        Booking booking2 = DataHelper.createBooking(cat2,startingTime2,endingTime2,u1,d,bookingRepository);
         HashMap<BookingCategory,Long> dailyStatistic = new HashMap<>();
         dailyStatistic.put(cat1, (endingTime1.getTime()-startingTime1.getTime()) / (1000*60*60));
         dailyStatistic.put(cat2, (endingTime2.getTime()-startingTime2.getTime()) / (1000*60*60));
         Assertions.assertEquals(dailyStatistic, productivityAnalysisService.getStatisicForCurrentUserByDay(1));
-
     }
 
     @DirtiesContext
     @Test
     @WithMockUser(username = "statistic_test_user1", authorities = {"EMPLOYEE"})
     public void statistic_user_backstepDays(){
-        //Data setup
-        User admin = DataHelper.createAdminUser("admin", userRepository);
-        User u1 = DataHelper.createUserWithRoles("statistic_test_user1", Sets.newSet(UserRole.EMPLOYEE), userRepository);
-        Dice d = DataHelper.createDice("testdice", null, admin, u1, diceRepository, raspberryPiRepository, roomRepository);
-        BookingCategory cat1 = DataHelper.createBookingCategory("test_category_01", admin, bookingCategoryRepository);
-        BookingCategory cat2 = DataHelper.createBookingCategory("test_category_02", admin, bookingCategoryRepository);
-
-        Booking b1 = new Booking();
-        b1.setDice(d);
-        b1.setBookingCategory(cat1);
         //set activity end time to 25 hours before current time.
         Date endingTime1 = new Date(new Date().getTime() - 60*1000*60*25);
-        b1.setActivityEndDate(endingTime1);
         //set activity start time to 30 hours ago
         Date startingTime1 = new Date(new Date().getTime() - 60*1000*60*30);
-        b1.setActivityStartDate(startingTime1);
-        try {
-            b1 = bookingService.saveBooking(b1);
-        } catch (ProdigaGeneralExpectedException e) {
-            e.printStackTrace();
-        }
+        Booking booking1 = DataHelper.createBooking(cat1,startingTime1,endingTime1,u1,d,bookingRepository);
 
-        Booking b2 = new Booking();
-        b2.setDice(d);
-        b2.setBookingCategory(cat2);
         //set activity end time to 5 minutes before current time.
         Date endingTime2 = new Date(new Date().getTime() - 60*1000*5);
-        b2.setActivityEndDate(endingTime2);
         //set activity start time to 30 minutes ago
         Date startingTime2 = new Date(new Date().getTime() - 60*1000*30);
-        b2.setActivityStartDate(startingTime2);
-        try {
-            b2 = bookingService.saveBooking(b2);
-        } catch (ProdigaGeneralExpectedException e) {
-            e.printStackTrace();
-        }
+        Booking booking2 = DataHelper.createBooking(cat2,startingTime2, endingTime2,u1,d,bookingRepository);
+
         HashMap<BookingCategory,Long> backstep1Statistic = new HashMap<>();
         backstep1Statistic.put(cat2, (endingTime2.getTime()-startingTime2.getTime()) / (1000*60*60));
         HashMap<BookingCategory,Long> backstep2Statistic = new HashMap<>();
@@ -205,26 +151,11 @@ public class ProductivityAnalysisServiceTest {
     @Test
     @WithMockUser(username = "statistic_test_user1", authorities = {"EMPLOYEE"})
     public void statistic_user_backstepWeek(){
-        //Data setup
-        User admin = DataHelper.createAdminUser("admin", userRepository);
-        User u1 = DataHelper.createUserWithRoles("statistic_test_user1", Sets.newSet(UserRole.EMPLOYEE), userRepository);
-        Dice d = DataHelper.createDice("testdice", null, admin, u1, diceRepository, raspberryPiRepository, roomRepository);
-        BookingCategory cat1 = DataHelper.createBookingCategory("test_category_01", admin, bookingCategoryRepository);
-
-        Booking b1 = new Booking();
-        b1.setDice(d);
-        b1.setBookingCategory(cat1);
         //set activity end time to 3 days 22 hours days before current time.
         Date endingTime1 = new Date(new Date().getTime() - 60*1000*60*24*4 + 60*1000*180);
-        b1.setActivityEndDate(endingTime1);
         //set activity start time to 4 days ago
         Date startingTime1 = new Date(new Date().getTime() - 60*1000*60*24*4);
-        b1.setActivityStartDate(startingTime1);
-        try {
-            b1 = bookingService.saveBooking(b1);
-        } catch (ProdigaGeneralExpectedException e) {
-            e.printStackTrace();
-        }
+        Booking booking1 = DataHelper.createBooking(cat1,startingTime1,endingTime1,u1,d,bookingRepository);
 
         HashMap<BookingCategory,Long> thisWeekStatistic = new HashMap<>();
         thisWeekStatistic.put(cat1, (endingTime1.getTime()-startingTime1.getTime()) / (1000*60*60));
