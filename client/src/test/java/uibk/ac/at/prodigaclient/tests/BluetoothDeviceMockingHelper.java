3a8de14f48c5dec83999a7848400374a1f8e7b38
Gabriel Mitterrutzner <Gabriel.Mitterrutzner@student.uibk.ac.at>
2020-05-12 23:46:47 +0200

    Improved mocking Bluetooth Device

--- client/src/test/java/uibk/ac/at/prodigaclient/tests/BluetoothDeviceMockingHelper.java
+++ client/src/test/java/uibk/ac/at/prodigaclient/tests/BluetoothDeviceMockingHelper.java
@@ -7,6 +7,7 @@ import tinyb.BluetoothGattService;
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -23,8 +24,6 @@ public class BluetoothDeviceMockingHelper {
     private static final String COMMANDWRITERCHARACTERISTICUUID = "f1196f54-71a4-11e6-bdf4-0800200c9a66";
     private static final String PASSWORDCHARACTERISTICUUID = "f1196f57-71a4-11e6-bdf4-0800200c9a66";
 
-    private static final String[] FLAGS = {"read", "write-without-response", "notify"};
-    private static final boolean NOTIFYING = true;
     private static final byte[] CUBEPASSWORD = {0x30, 0x30, 0x30, 0x30, 0x30, 0x30};
     private static final byte[] READHISTORYCMD = {0x01};
     private static final byte[] DELETEHISTORYCMD = {0x02};
@@ -43,9 +42,9 @@ public class BluetoothDeviceMockingHelper {
         when(bluetoothDevice.getName()).thenReturn(DEVICE_NAME);
         when(bluetoothDevice.getAddress()).thenReturn(DEVICE_MAC);
 
-        when(bluetoothDevice.connect()).thenReturn(true);
-        when(bluetoothDevice.getConnected()).thenReturn(true);
-        when(bluetoothDevice.getConnected()).thenReturn(true);
+        when(bluetoothDevice.connect()).thenReturn(connectToCube());
+        when(bluetoothDevice.getConnected()).thenReturn(connected);
+        when(bluetoothDevice.disconnect()).thenReturn(disconnectFromCube());
 
         List<BluetoothGattService> serviceList = new LinkedList<>();
         serviceList.add(bluetoothBatteryService);
@@ -75,42 +74,78 @@ public class BluetoothDeviceMockingHelper {
         when(bluetoothPasswordCharacteristic.getUUID()).thenReturn(PASSWORDCHARACTERISTICUUID);
         when(bluetoothBatteryCharacteristic.getUUID()).thenReturn(BATTERYCHARACTERISTICUUID);
 
-        when(bluetoothBatteryCharacteristic.readValue()).thenReturn(new byte[]{0x45});
+        when(bluetoothBatteryCharacteristic.readValue()).thenReturn(new byte[]{0x45}); // you can always get the Bluetooth percentage
 
-        when(bluetoothPasswordCharacteristic.writeValue(CUBEPASSWORD)).thenReturn(true);
+        when(bluetoothPasswordCharacteristic.writeValue(CUBEPASSWORD)).thenReturn(insertPassword());
 
         when(bluetoothCommandWriteCharacteristics.writeValue(READHISTORYCMD)).thenReturn(true);
         when(bluetoothCommandWriteCharacteristics.writeValue(DELETEHISTORYCMD)).thenReturn(true);
 
         when(bluetoothCommandReadCharacteristics.readValue()).thenReturn(flipBetweenHistory());
-        when(bluetoothCurrentFacetCharacteristics.readValue()).thenReturn(new byte[]{0x01});
+        when(bluetoothCurrentFacetCharacteristics.readValue()).thenReturn(getCurrentFacet());
         return bluetoothDevice;
     }
 
-    private static int historyRead = 0;
+    private static int historyRead = -1;
+    private static boolean hasInseartedPW = false;
+    private static boolean connected = false;
+
+    private static boolean insertPassword() {
+        historyRead = 0;
+        hasInseartedPW = true;
+        return true;
+    }
+
+    private static boolean connectToCube() {
+        hasInseartedPW = false;
+        connected = true;
+        return true;
+    }
+
+    private static boolean disconnectFromCube() {
+        hasInseartedPW = false;
+        connected = false;
+        return true;
+    }
+
+    private static byte[] getCurrentFacet() {
+        byte[] returnValue;
+
+        if (hasInseartedPW) {
+            returnValue = new byte[]{0x01};
+        } else {
+            returnValue = new byte[]{};
+        }
+
+        return returnValue;
+    }
 
     private static byte[] flipBetweenHistory() {
         byte[] returnValue;
-        switch (historyRead){
-            case 0:
-                historyRead = 1;
-                returnValue = new byte[] {0x0d, 0x00, 0x18, 0x3c, 0x00, 0x04, 0x10, 0x00, 0x04, 0x10, 0x00, 0x08,
-                        0x16, 0x00, 0x0c, 0x32, 0x00, 0x10, 0x0d, 0x00, 0x14};
-                break;
-            case 1:
-                historyRead = 2;
-                returnValue = new byte[] {0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-                break;
-            case 2:
-                historyRead = 0;
-                returnValue = new byte[] {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-                break;
-            default:
-                historyRead = 0;
-                returnValue = new byte[]{};
-                break;
+
+        if(hasInseartedPW) {
+            switch (historyRead) {
+                case 0:
+                    historyRead = 1;
+                    returnValue = new byte[]{0x0d, 0x00, 0x18, 0x3c, 0x00, 0x04, 0x10, 0x00, 0x04, 0x10, 0x00, 0x08,
+                            0x16, 0x00, 0x0c, 0x32, 0x00, 0x10, 0x0d, 0x00, 0x14};
+                    break;
+                case 1:
+                    historyRead = 2;
+                    returnValue = new byte[]{0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+                    break;
+                case 2:
+                    historyRead = 0;
+                    returnValue = new byte[]{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+                    break;
+                default:
+                    returnValue = new byte[]{};
+                    break;
+            }
+        } else {
+            returnValue = new byte[]{};
         }
 
         return returnValue;
