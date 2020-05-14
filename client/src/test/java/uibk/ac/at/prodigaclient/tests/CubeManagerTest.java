8133400151b50f4906747725c7f5a181c82024cd
Gabriel Mitterrutzner <Gabriel.Mitterrutzner@student.uibk.ac.at>
2020-05-14 21:50:01 +0200

    Externalised mocking of Cube Manager

--- client/src/test/java/uibk/ac/at/prodigaclient/tests/CubeManagerTest.java
+++ client/src/test/java/uibk/ac/at/prodigaclient/tests/CubeManagerTest.java
@@ -17,35 +17,11 @@ import static org.mockito.Mockito.when;
 
 public class CubeManagerTest {
 
-    @Mock
     private CubeManager cubeManagerMock;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        Set<String> set = new HashSet<>();
-        set.add("0C:61:CF:C7:8F:D5");
-        set.add("0C:61:CF:C7:8F:D6");
-
-        when(cubeManagerMock.getCubeIDList()).thenReturn(set); // addet a set of cubes
-
-        when(cubeManagerMock.getCurrentSide("0C:61:CF:C7:8F:D5")).thenReturn(1); // cube 1 current side = 1
-        when(cubeManagerMock.getCurrentSide("0C:61:CF:C7:8F:D6")).thenReturn(12); // cube 2 current side = 12
-
-        when(cubeManagerMock.getBattery("0C:61:CF:C7:8F:D5")).thenReturn(45); // cube 1 battery = 45
-        when(cubeManagerMock.getBattery("0C:61:CF:C7:8F:D6")).thenReturn(90); // cube 2 battery = 90
-
-        List<HistoryEntry> historyEntryListOne = new LinkedList<>();
-        List<HistoryEntry> historyEntryListTwo = new LinkedList<>();
-
-        historyEntryListOne.add(new HistoryEntry(new byte[]{0x0d, 0x00, 0x18})); // Side 6 -> Seconds 13
-        historyEntryListOne.add(new HistoryEntry(new byte[]{0x3c, 0x00, 0x04})); // Side 1 -> Seconds 60
-
-        historyEntryListTwo.add(new HistoryEntry(new byte[]{0x10, 0x00, 0x04})); // Side 1 -> Seconds 16
-        historyEntryListTwo.add(new HistoryEntry(new byte[]{0x10, 0x00, 0x08})); // Side 2 -> Seconds 16
-
-        when(cubeManagerMock.getHistory("0C:61:CF:C7:8F:D5")).thenReturn(historyEntryListOne); // cube 1 history list one
-        when(cubeManagerMock.getHistory("0C:61:CF:C7:8F:D6")).thenReturn(historyEntryListTwo); // cube 2 history list two
+        cubeManagerMock = CubeManagerMockCreator.mockFullCubeManager();
     }
 
     @Test
