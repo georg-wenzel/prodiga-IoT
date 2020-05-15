710069719b5fc22a757bbbcd8db078b0b743d221
Gabriel Mitterrutzner <Gabriel.Mitterrutzner@student.uibk.ac.at>
2020-05-15 23:24:44 +0200

    Last commit for today.
    Only tested things

---
+++ client/script/script/newtest.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# This works
+sed '/5/ a #My inserted line' ./test.txt
--- client/script/script/test.sh
+++ client/script/script/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 filename=./bluetooth.conf
-sed '/<policy user="root">(.|\n)*?<\/policy>/i \ line10/g' $filename
+sed '/<policy user="root">(.|\n)*?<\/policy>/a line10' $filename
