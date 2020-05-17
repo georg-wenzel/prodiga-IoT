1e96915ba7fc861d75d6d429473de3e123ba2e89
Georg Felix Wenzel <georg.wenzel@student.uibk.ac.at>
2020-05-18 00:59:22 +0200

    Quickfix data.sql: Admin is not teamleader (he is not assigned to a team, creates invalid data state)

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,7 +1,6 @@
 INSERT INTO user (enabled, first_name, last_name, password, username, create_user_username, create_date) VALUES(TRUE, 'Admin', 'Istrator', '$2a$10$d8cQ7Euz2hM43HOHWolUGeCEZSS/ltJVJYiJAmczl1X5FKzCjg6PC', 'admin', 'admin', '2016-01-01 00:00:00');
 INSERT INTO user_user_role (user_username, ROLES) VALUES ('admin', 'ADMIN');
 INSERT INTO user_user_role (user_username, ROLES) VALUES ('admin', 'EMPLOYEE');
-INSERT INTO user_user_role (user_username, ROLES) VALUES ('admin', 'TEAMLEADER');
 
 INSERT INTO booking_category (id, name, object_created_user_username, object_created_date_time, object_changed_user_username, object_changed_date_time) VALUES (1, 'Pause / Vacation', 'admin', NOW(), 'admin', NOW());
 INSERT INTO booking_category (id, name, object_created_user_username, object_created_date_time, object_changed_user_username, object_changed_date_time) VALUES (2, 'Conceptualizing', 'admin', NOW(), 'admin', NOW());
